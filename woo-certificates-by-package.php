8799cb8fe7807aaeb507a59e0608745ae80d0ddd
Junnior Rivas <jrdrivasg@gmail.com>
2025-08-20 14:59:06 -0400

    First commit

---
+++ woo-certificates-by-package.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       WooCommerce Certificates by Package
+ * Plugin URI:        https://edusof/woo-certificates-by-package
+ * Description:       Generates and manages certificates as products in WooCommerce.
+ * Version:           1.0.0
+ * Author:            Edusof
+ * Author URI:        https://edusof.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       woo-certificates-by-package
+ * Domain Path:       /languages
+ */
+
+// Si este archivo es accedido directamente, aborta.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
