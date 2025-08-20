26dab3358a8f1a7d475e8f8cc676435fe422a7a2
Junnior Rivas <jrdrivasg@gmail.com>
2025-08-20 15:02:59 -0400

    Fixes

--- woo-certificates-by-package.php
+++ woo-certificates-by-package.php
@@ -8,7 +8,7 @@
  * Author URI:        https://edusof.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       woo-certificates-by-package
+ * Text Domain:       woocertificatespackage
  * Domain Path:       /languages
  */
 
